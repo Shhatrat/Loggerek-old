7cf3caf4e542d8db1fef51febb8ea09f9b4698d4
Szymon Kaminski <szymonkaminski.ptb@gmail.com>
2017-07-10 21:10:14 +0000

    fix log adapter

--- app/src/main/java/com/shhatrat/loggerek/adapters/LogAdapter.kt
+++ app/src/main/java/com/shhatrat/loggerek/adapters/LogAdapter.kt
@@ -1,5 +1,7 @@
 package com.shhatrat.loggerek.adapters
 
+import android.app.Activity
+import android.app.getKoin
 import android.content.Context
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.RecyclerView
@@ -9,17 +11,68 @@ import android.view.ViewGroup
 import com.afollestad.materialdialogs.MaterialDialog
 import com.shhatrat.loggerek.R
 import com.shhatrat.loggerek.fragments.LogFragment
+import com.shhatrat.loggerek.models.SingleLog
 import com.shhatrat.loggerek.models.Unsend
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
+import io.realm.Realm
+import kotlinx.android.synthetic.main.activity_full_log.view.*
 import kotlinx.android.synthetic.main.list_layout.view.*
 import kotlin.collections.ArrayList
 
 
-class LogAdapter(var c: Context, var lists: ArrayList<String?>, var type : LogFragment.Type) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+class LogAdapter(var c: Activity, var lists: ArrayList<String?>, var type : LogFragment.Type) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+
+    val realm by lazy{c.getKoin().get<Realm>()}
 
     fun getList(): ArrayList<String?> {
         return lists
+    }
+
+    fun removeAt(id : Int){
+        removeFromDb(lists.get(id))
+        lists.removeAt(id)
+        notifyItemRemoved(id)
+        notifyItemRangeChanged(id, lists.size)
+    }
+
+    private fun  removeFromDb(get: String?) {
+        realm.beginTransaction()
+        realm.where(SingleLog::class.java)
+                .equalTo("type", type.name)
+                .equalTo("log", get)
+                .findAll().deleteAllFromRealm()
+        realm.commitTransaction()
+    }
+
+    private fun  editLog(get: String?, position: Int){
+        MaterialDialog.Builder(c)
+                .title("Edit log")
+                .positiveText("ok")
+                .negativeText("cancel")
+                .neutralText("delete")
+                .neutralColor(ContextCompat.getColor(c, R.color.md_red_400))
+                .onNeutral { dialog, which -> run { removeAt(position); } }
+                .inputType(android.text.InputType.TYPE_CLASS_TEXT)
+                .input(get, get, com.afollestad.materialdialogs.MaterialDialog.InputCallback { dialog, input -> updateToDb(get ,input.toString(), position) })
+                .show()
+    }
+
+    private fun  saveToDb(log: String) {
+        val toAdd = SingleLog()
+        toAdd.saveEnum(type)
+        toAdd.log = log
+        realm.beginTransaction()
+        realm.copyToRealm(toAdd)
+        realm.commitTransaction()
+     }
+
+    private fun  updateToDb(get: String?, toString: String, position: Int) {
+        removeFromDb(get)
+        saveToDb(toString)
+        lists[position] = toString
+        notifyDataSetChanged()
+        notifyItemChanged(position)
     }
 
     fun getClickListener(): Observable<String> {
@@ -39,6 +92,7 @@ class LogAdapter(var c: Context, var lists: ArrayList<String?>, var type : LogFr
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
         (holder as Item).bindData(lists[position]!!, type)
+        holder.itemView.list_linear_layout.setOnClickListener { editLog(lists[position]!!, position) }
     }
 
     class Item(itemView: View) : RecyclerView.ViewHolder(itemView) {
--- app/src/main/java/com/shhatrat/loggerek/fragments/LogFragment.kt
+++ app/src/main/java/com/shhatrat/loggerek/fragments/LogFragment.kt
@@ -7,17 +7,20 @@ import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.helper.ItemTouchHelper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.afollestad.materialdialogs.MaterialDialog
 import com.shhatrat.loggerek.R
 import com.shhatrat.loggerek.adapters.LogAdapter
+import com.shhatrat.loggerek.adapters.UnsendAdapter
 import com.shhatrat.loggerek.models.SingleLog
 import io.github.codefalling.recyclerviewswipedismiss.SwipeDismissRecyclerViewTouchListener
 import io.reactivex.disposables.Disposable
 import io.realm.Realm
 import kotlinx.android.synthetic.main.fragment_log.*
+import kotlinx.android.synthetic.main.fragment_unsend.*
 
 class LogFragment : Fragment() {
 
@@ -25,7 +28,6 @@ class LogFragment : Fragment() {
         GOOD, BAD, DEFAULT
     }
 
-    private var subscribe: Disposable? = null
     lateinit var type : Type
     val realm by lazy{activity.getKoin().get<Realm>()}
 
@@ -63,8 +65,23 @@ class LogFragment : Fragment() {
         refreshDataAndRecycleView()
     }
 
+
+    fun getHelper(recyclerView : RecyclerView): ItemTouchHelper.SimpleCallback {
+        var simpleTouch = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT){
+            override fun onMove(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?, target: RecyclerView.ViewHolder?): Boolean {
+                return  true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder?, direction: Int) {
+                val adapter =  recyclerView.adapter as LogAdapter
+                adapter.removeAt(viewHolder!!.adapterPosition)
+            }
+        }
+        return simpleTouch
+    }
+
     fun refreshDataAndRecycleView(){
-        log_recycle_view.adapter = LogAdapter(this.context, getList(), type)
+        log_recycle_view.adapter = LogAdapter(this.activity, getList(), type)
         log_recycle_view.adapter.notifyDataSetChanged()
         getList()
     }
@@ -72,8 +89,8 @@ class LogFragment : Fragment() {
     private fun setupRecycleView() {
         log_recycle_view.layoutManager = LinearLayoutManager(this.context)
         log_recycle_view.hasFixedSize()
-        log_recycle_view.setOnTouchListener(preapreListener(log_recycle_view))
-        log_recycle_view.adapter = LogAdapter(this.context, getList(), type)
+        ItemTouchHelper(getHelper(log_recycle_view)).attachToRecyclerView(log_recycle_view)
+        log_recycle_view.adapter = LogAdapter(this.activity, getList(), type)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -97,62 +114,5 @@ class LogFragment : Fragment() {
             fr.arguments = args
             return fr
         }
-    }
-
-    fun preapreListener(recyclerView :RecyclerView): SwipeDismissRecyclerViewTouchListener? {
-
-        return SwipeDismissRecyclerViewTouchListener.Builder(
-                recyclerView,
-                object : SwipeDismissRecyclerViewTouchListener.DismissCallbacks{
-                    override fun onDismiss(view: View?) {
-                        val id = recyclerView.getChildAdapterPosition(view)
-                        val adapter =  recyclerView.adapter as LogAdapter
-                        removeFromDb(adapter.lists.get(id))
-                        adapter.lists.removeAt(id)
-                        log_recycle_view.adapter = LogAdapter(this@LogFragment.context, getList(), type)
-                    }
-
-                    override fun canDismiss(p0: Int): Boolean {
-                        return true
-                    }
-                })
-                .setItemClickCallback({
-                    u ->
-                    run {
-                        try {
-                            val adapter = recyclerView.adapter as LogAdapter
-                            editLog(adapter.lists[u])
-                        }catch (e :Throwable){}
-                    }
-                })
-                .setIsVertical(false)
-                .create()
-    }
-
-    private fun  removeFromDb(get: String?) {
-        realm.beginTransaction()
-        realm.where(SingleLog::class.java)
-                .equalTo("type", type.name)
-                .equalTo("log", get)
-                .findAll().deleteAllFromRealm()
-        realm.commitTransaction()
-    }
-
-    private fun  editLog(get: String?){
-        MaterialDialog.Builder(this.context)
-                .title("Edit log")
-                .positiveText("ok")
-                .negativeText("cancel")
-                .neutralText("delete")
-                .neutralColor(ContextCompat.getColor(context, R.color.md_red_400))
-                .onNeutral { dialog, which -> run { removeFromDb(get); refreshDataAndRecycleView() } }
-                .inputType(android.text.InputType.TYPE_CLASS_TEXT)
-                .input(get, get, com.afollestad.materialdialogs.MaterialDialog.InputCallback { dialog, input -> updateToDb(get ,input.toString()) })
-                .show()
-    }
-
-    private fun  updateToDb(get: String?, toString: String) {
-        removeFromDb(get)
-        saveToDb(toString)
     }
 }
